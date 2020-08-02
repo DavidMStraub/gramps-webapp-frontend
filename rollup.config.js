c43d9ca5bc13b1e92e16ecf570b083b4f3d5d596
David Straub <david.straub@tum.de>
2020-08-02 18:04:39 +0200

    Get rid of env.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import merge from 'deepmerge';
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
+import replace from '@rollup/plugin-replace';
 
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
@@ -24,7 +25,11 @@ export default merge(baseConfig, {
   // if you use createSpaConfig, you can use your index.html as entrypoint,
   // any <script type="module"> inside will be bundled by rollup
   input: './index.html',
-
+  plugins: [
+    replace({
+      'window.APIHOST': JSON.stringify('')
+    })
+  ]
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
   // input: './app.js',
